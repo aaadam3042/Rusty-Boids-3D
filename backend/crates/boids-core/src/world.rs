5932410770ce8affce52bb7b948bb600ba9c74aa
aaadam3042 <aaadam3042@gmail.com>
2026-08-10 15:25:46 +1000

    Integrated the boids system into world

--- backend/crates/boids-core/src/world.rs
+++ backend/crates/boids-core/src/world.rs
@@ -1,41 +1,66 @@
 use crate::boid::Boid;
 use crate::bounds::{BoundaryMode, Bounds};
 use crate::params::SimulationParams;
+use crate::spawn::{SpawnConfig, spawn_boids};
+use crate::steering::acceleration_for;
 
-struct World {
-    boids: Vec<Boid>,
+pub struct WorldSettings {
     params: SimulationParams,
     bounds: Bounds,
-    bounds_mode: BoundaryMode,
-    elapsed: f32,
+    boundary_mode: BoundaryMode,
+}
+
+pub struct World {
+    boids: Vec<Boid>,
+    settings: WorldSettings,
 }
 
 impl World {
-    pub fn new() -> Self {
-
+    pub fn new(boids: Vec<Boid>, settings: WorldSettings) -> Self {
+        Self {
+            boids,
+            settings
+        }
     }
 
-    pub fn from_config(config: Config) {
-
+    pub fn from_config(spawn_config: SpawnConfig, settings: WorldSettings) -> Self {
+        let boids = spawn_boids(&spawn_config, &settings.bounds);
+        Self::new(boids, settings)
     }
 
-    pub fn seeded() -> Self {
+    pub fn step(&mut self, dt: f32) {
+        let mut accelerations = Vec::with_capacity(self.boids.len());
 
-    }
+        // Step 1: Calculate the acceleration forces on each boid
+        for index in 0..self.boids.len() {
+            accelerations.push(acceleration_for(index, &self.boids, &self.settings.params));
+        }
 
-    pub fn step(&mut self) {
+        let max_speed = self.settings.params.max_speed();
 
+        // Step 2: update each boid
+        for (boid, acceleration) in self.boids.iter_mut().zip(accelerations) {
+            boid.velocity += acceleration * dt;
+            boid.velocity = boid.velocity.limit_length(max_speed);
+           
+           boid.position += boid.velocity * dt;
+
+            match &self.settings.boundary_mode {
+                BoundaryMode::Wrap => {
+                    self.settings.bounds.apply_wrap(&mut boid.position);
+                }
+                BoundaryMode::Bounce => {
+                    self.settings.bounds.apply_bounce(&mut boid.position, &mut boid.velocity);
+                }
+            }
+
+            // TODO: Remove once we implment z axis
+            boid.position.z = 0.0;
+            boid.velocity.z = 0.0;
+        }
     }
 
     pub fn boids(&self) -> &[Boid] {
         &self.boids
-    }
-
-    pub fn params(&self) -> &SimulationParams {
-        &self.params
-    }
-
-    pub fn bounds(&self) -> Bounds {
-        self.bounds
     }
 }
